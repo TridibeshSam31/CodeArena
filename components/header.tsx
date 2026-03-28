69a895126ec9e91094596b84470b7dd0ea53a4e9
TridibeshSam31 <tridisam651@gmail.com>
2026-03-28 20:40:05 +0530

    header component done

--- components/header.tsx
+++ components/header.tsx
@@ -2,11 +2,22 @@ import React from 'react'
 
 const header = () => {
   return (
-    <header className='flex justify-between items-center'>
+    <header className='flex justify-between items-center px-6 py-3 border-b bg-gray-900'>
 
-     <div>
-        <div className='w-9 h-9 bg-blue-500'></div>
-        <h1>CodeArena</h1>
+
+      {/*left side */}
+      <div className='flex items-center gap-3'>
+        <div className='w-9 h-9 bg-blue-500'> Logo   </div>
+       <div>
+         <h1 className='text-lg font-bold text-white'>CodeArena</h1>
+         <p className='text-xs text-gray-400'>Run Your code Smoothly</p>
+       </div>
+      </div>
+
+     {/*now status i.e online or offline , Right Side */}
+     <div className='flex items-center gap-2 text-sm text-gray-400'>
+      <span></span>
+      <span >Online</span>
      </div>
     </header>
   )
