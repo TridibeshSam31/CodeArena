85d3976120c0e99960b64142b7f3484768eb92a8
TridibeshSam31 <tridisam651@gmail.com>
2026-05-10 11:56:21 +0530

    wrtiting main page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,1 @@
-"use client"
-import Image from "next/image";
-import Header from "@/components/header";
-import StdinInput from "@/components/stdinInput";
-import CodeEditor from "@/components/CodeEditor";
-export default function Home() {
-  return (
-    <div>
-      <Header/>
-      <StdinInput value="" onChange={(e)=>console.log(e)}/>
-    </div>
-    
-    
-    
-    
-  );
-}
+"use client "
